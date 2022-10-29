b85ba33b92c7acabea9fe64e7e0102f7c70ee650
Vlad Shmaliukh <zskat02@gmail.com>
2022-10-29 22:01:29 +0300

    Clean up code for config file

--- src/main/java/com/nonamed/nonamedgame/Config.java
+++ src/main/java/com/nonamed/nonamedgame/Config.java
@@ -10,33 +10,13 @@ import static com.nonamed.nonamedgame.StaticData.*;
 
 public final class Config {
 
+    public static final Random RANDOM = new Random();
     public static boolean isUsik = true;
-
     public static Image HERO_ANIMATED;
     public static Image HERO_FIGHT_LEFT;
     public static Image HERO_FIGHT_RIGHT;
     public static Image HERO_AVATAR;
-
-    public static void setUpProfileConfig(){
-        if(isUsik){
-            HERO_NAME_STR = "УСИК";
-            HERO_ANIMATED = USIK_ANIMATED;
-            HERO_FIGHT_LEFT = USIK_FIGHT_LEFT;
-            HERO_FIGHT_RIGHT = USIK_FIGHT_RIGHT ;
-            HERO_AVATAR = USIK_AVATAR;
-        } else {
-            HERO_ANIMATED = OLHA_ANIMATED;
-            HERO_FIGHT_LEFT = OLHA_FIGHT_LEFT;
-            HERO_FIGHT_RIGHT = OLHA_FIGHT_RIGHT ;
-            HERO_AVATAR = OLHA_AVATAR;
-            HERO_NAME_STR = "ХАРЛАН";
-        }
-    }
-
-    public static final Random RANDOM = new Random();
-
     public static double VOLUME_LEVEL = 0.5;
-
     public static int WINDOW_HEIGHT = 1080;
     public static int WINDOW_WIDTH = 1920;
     public static int WORLD_HEIGHT = 2560;
@@ -46,18 +26,13 @@ public final class Config {
     public static int HERO_DAMAGE = 5;
     public static int HERO_HEALTH = 10000;
     public static int HERO_ENERGY = 10000;
-
     public static int DARK_PERSON_SPEED = 3;
     public static int ENEMY_DAMAGE = 3;
     public static List<String> NAME_LIST = new ArrayList<>();
-
-
-
     public static String RIGHT_HERO_KICK = "X";
     public static String ENEMY_NAME = "!*!";
     public static int ENEMY_HEALTH = 3000;
     public static int ENEMY_SPEED = 3;
-
 
     static {
         NAME_LIST.add("ВЛАД");
@@ -145,6 +120,22 @@ public final class Config {
         NAME_LIST.add("ЯНГОЛ");
         NAME_LIST.add("DOOMGUY");
         NAME_LIST.add("TUX");
+    }
+
+    public static void setUpProfileConfig() {
+        if (isUsik) {
+            HERO_NAME_STR = "УСИК";
+            HERO_ANIMATED = USIK_ANIMATED;
+            HERO_FIGHT_LEFT = USIK_FIGHT_LEFT;
+            HERO_FIGHT_RIGHT = USIK_FIGHT_RIGHT;
+            HERO_AVATAR = USIK_AVATAR;
+        } else {
+            HERO_ANIMATED = OLHA_ANIMATED;
+            HERO_FIGHT_LEFT = OLHA_FIGHT_LEFT;
+            HERO_FIGHT_RIGHT = OLHA_FIGHT_RIGHT;
+            HERO_AVATAR = OLHA_AVATAR;
+            HERO_NAME_STR = "ХАРЛАН";
+        }
     }
 
 }
